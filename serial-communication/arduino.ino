4ad0d8cb0997de4730ad36af4d74de9157cbbe41
DDCcarr1 <ddccarr1@icloud.com>
2026-05-29 15:24:35 -0400

    Update arduino.ino

--- serial-communication/arduino.ino
+++ serial-communication/arduino.ino
@@ -1,21 +1,42 @@
-const byte led = 13;
-char input;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+String received;
 
 void setup() {
-  pinMode(led, OUTPUT);
   Serial.begin(9600);
+  lcd_start();
 }
 
 void loop() {
   if(Serial.available() > 0){
-      input = Serial.read();
+      received = Serial.readStringUntil('\n');
 
-      if(input == '1'){
-        digitalWrite(led, HIGH);
-        Serial.println("ON");
-      } else if(input == '0') {
-        digitalWrite(led, LOW);
-        Serial.println("OFF");
+      if (received != ""){
+        lcd_reset();
+        lcd.print(received);
+        Serial.print(received);
       }
   }
+}
+
+void lcd_start(){
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+  lcd.noCursor();
+  lcd.home();
+  lcd.display();
+  lcd.noBlink();
+}
+
+void lcd_reset(){
+  lcd.clear();
+  lcd.backlight();
+  lcd.noCursor();
+  lcd.home();
+  lcd.display();
+  lcd.noBlink();
 }
