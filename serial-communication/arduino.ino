c54a53cb9384e19e8d80e083972267a06485cb8d
DDCcarr1 <ddccarr1@icloud.com>
2026-06-01 15:27:47 -0400

    JSON to prepare for API
    Refactor serial input handling to use JSON parsing and improve LCD display functionality.

--- serial-communication/arduino.ino
+++ serial-communication/arduino.ino
@@ -1,26 +1,40 @@
+#include <ArduinoJson.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-String received;
+JsonDocument json;
+String inputData;
 
 void setup() {
   Serial.begin(9600);
+
   lcd_start();
 }
 
 void loop() {
   if(Serial.available() > 0){
-      received = Serial.readStringUntil('\n');
+      inputData = Serial.readStringUntil('\n');
+      json.clear();
 
-      if (received != ""){
+      DeserializationError error = deserializeJson(json, inputData);
+
+      if(error) {
+        lcd.print("JSON Error");
+        Serial.println("Error: JSON Parsing Failed");
+        return;
+      }
+  
+      const char* text = json["text"];
+      if (text != nullptr && String(text) != ""){
         lcd_reset();
-        lcd.print(received);
-        Serial.print(received);
+        lcd.print(text);
+        Serial.print(text);
+        Serial.println(" is now being displayed.");
       }
   }
 }
+
 
 void lcd_start(){
   lcd.init();
@@ -34,9 +48,5 @@ void lcd_start(){
 
 void lcd_reset(){
   lcd.clear();
-  lcd.backlight();
-  lcd.noCursor();
   lcd.home();
-  lcd.display();
-  lcd.noBlink();
 }
