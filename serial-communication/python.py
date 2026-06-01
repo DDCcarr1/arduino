c35eb65d973dc7aa58ca87706ce23f80a5f12127
DDCcarr1 <ddccarr1@icloud.com>
2026-06-01 15:26:57 -0400

    Use JSON to prepare for API
    Refactor communication to send JSON payload instead of plain text. Added character limit check and improved response handling.

--- serial-communication/python.py
+++ serial-communication/python.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import json
 
 #port = 'COM5'
 #port = '/dev/cu.usbserial-1420'
@@ -16,28 +17,32 @@ try:
     while True:
         comm = str(input(f"Enter a string with a character limit of {maxChar} or 'q' to quit.\n\t").strip())
 
-        if len(comm) > maxChar:
+        if comm == "q":
+            break
+
+        elif len(comm) > maxChar:
             print(f"Too many characters were entered. The maximum limit is {maxChar}.")
             continue
 
         elif len(comm) == 0:
-            print(f"No characters were entered. The maximum limit is {maxChar}.")
-
-        elif comm == "q":
-            break
+            continue
 
         else:
-            print(f"Sending {comm} to the Arduino to print.")
-            ser.write((comm + "\n").encode())
-            time.sleep(0.1)
+            payload = {"text": comm}
+            json_string = json.dumps(payload) + "\n"
+
+            print(f"Sending JSON payload of {json_string.strip()}")
+            ser.write(json_string.encode('utf-8'))
             timeout_start = time.time()
-            while ser.in_waiting == 0:
-                if time.time() - timeout_start > 2:  # 2-second timeout
-                    print(f"Failed to send {comm} to the Arduino to print.")
+            while ser.in_waiting > 0:
+                if time.time() - timeout_start > 5:
+                    print("Error: The Arduino failed to respond within 5 seconds.")
                     break
 
             response = ser.readline().decode('utf-8').strip()
-            print(f"Sent {response} to the Arduino to print.")
+            print(f"{response}")
+            print()
+            continue
 
 except serial.SerialException as error:
     print(f"The connection to port {port} has failed. Ensure the Arduino is plugged in and the Serial Monitor in the Arduino IDE is closed, then try again.\n{error}")
